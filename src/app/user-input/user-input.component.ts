cdcd9b29eb4763f2a11c4f1cd1126dadf6bc37f4
Alaa Ahmed <alaa0507932744@gmail.com>
2026-02-19 21:45:21 +0200

    I added a type definition on investment-input.model.ts file and improt the type to app component and user-input component

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+import { FormsModule } from '@angular/forms'
+import { type InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit(){
+    this.calculate.emit({
+      initialInvestment: parseFloat(this.enteredInitialInvestment),
+      duration: parseFloat(this.enteredDuration),
+      expectedReturn: parseFloat(this.enteredExpectedReturn),
+      annualInvestment: parseFloat(this.enteredAnnualInvestment),
+    });
+  }
+}
