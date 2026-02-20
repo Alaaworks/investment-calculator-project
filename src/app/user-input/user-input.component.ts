09a2f06248f62b1d2577095777482dff541d43c2
Alaa Ahmed <alaa0507932744@gmail.com>
2026-02-20 15:07:03 +0200

    Finally i finished the investment calculator project

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,30 +1,34 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
-import { FormsModule } from '@angular/forms'
-import { type InvestmentInput } from '../investment-input.model';
+import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
 
-  @Output() calculate = new EventEmitter<InvestmentInput>();
+  constructor(private investmentService: InvestmentService) {}
 
-  enteredInitialInvestment = '0';
-  enteredAnnualInvestment = '0';
-  enteredExpectedReturn = '5';
-  enteredDuration = '10';
-
-  onSubmit(){
-    this.calculate.emit({
-      initialInvestment: parseFloat(this.enteredInitialInvestment),
-      duration: parseFloat(this.enteredDuration),
-      expectedReturn: parseFloat(this.enteredExpectedReturn),
-      annualInvestment: parseFloat(this.enteredAnnualInvestment),
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: parseFloat(this.enteredInitialInvestment()),
+      duration: parseFloat(this.enteredDuration()),
+      expectedReturn: parseFloat(this.enteredExpectedReturn()),
+      annualInvestment: parseFloat(this.enteredAnnualInvestment()),
     });
+
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 }
